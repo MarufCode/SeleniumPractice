7445dc9555d43e9136738787cd9f2ff9f8647e29
Maruf <marufsherikar32@gmail.com>
2025-11-10 17:43:14 +0530

    Selenium Practice New Repo

---
+++ src/test/java/org/example/TC02.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC02 {
+
+    @Test
+    @Description("Verify the title of THETestingAcademy Website.")
+    public void vwoLogin(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+        driver.close();
+
+    }
+}
